576995d720608f5643b5ff099bbc1e4bbbca2f5e
Kenneth <kyleabs20@gmail.com>
2020-12-03 21:00:17 -0800

    blah blah blah

--- index3.js
+++ index3.js
@@ -7,4 +7,27 @@ Student1.setDepartment("College of basic and applied science");
 Student1.setGraduated(false)
 Student1.setCollege("Sciences")
 
-console.log(`the first student in the university is ${Student1.getName()}`)
+const Student2 = new Schema.Student();
+Student2.setId(10669038);
+Student2.setName("Karen");
+Student2.setDepartment("College of basic and applied science");
+Student2.setGraduated(true)
+Student2.setCollege("Sciences")
+
+const Student3 = new Schema.Student();
+Student3.setId(10609438);
+Student3.setName("Kelvin");
+Student3.setDepartment("College of basic and applied science");
+Student3.setGraduated(true)
+Student3.setCollege("Sciences")
+
+const students = new Schema.Students();
+students.addStudents(Student1)
+students.addStudents(Student2)
+students.addStudents(Student3)
+
+
+// finding the bytes of the things
+
+
+
