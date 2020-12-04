7525002e187bbeb1093ecdb306b4dd5d5c55b986
Kenneth <kyleabs20@gmail.com>
2020-12-03 20:16:58 -0800

    working progress

--- index3.js
+++ index3.js
@@ -1,1 +1,10 @@
 const Schema = require('./students_pb');
+
+const Student1 = new Schema.Student();
+Student1.setId(10659488);
+Student1.setName("Kenneth");
+Student1.setDepartment("College of basic and applied science");
+Student1.setGraduated(false)
+Student1.setCollege("Sciences")
+
+console.log(`the first student in the university is ${Student1.getName()}`)
